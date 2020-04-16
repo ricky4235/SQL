56c03e94cb410bb2a8fb5e8b905cdd09acebc653
ricky <ricky1229x@gmail.com>
2020-04-16 17:07:48 +0800

    20200416.c

--- test.sql
+++ test.sql
@@ -1,18 +1,29 @@
-SELECT *
-FROM genius_data_202002
-where Department = 'SF1' & 'SF3'
-and Year = '2020'
-and Month = '2'
-and LargeCat = 'Mouse'
-and MediumCat = 'Wireless Mouse' or 'Gaming KB'
-and Description = 'OBM';
+SELECT d.`2nd Item Number` "Item Num" , d.`Customer Name`, d.Description,	#含空格欄位須用反引號  
+	   d.`Foreign Extended Price`  ,  d.`Quantity Shipped`
+       
+FROM genius_data.genius_data_202002 as d
+where `Department` = 'SF1' & 'SF3'
+and `Year` = '2020'
+and `Month` = '2'
+and `LargeCat` = 'Mouse'
+and `MediumCat` = 'Wireless Mouse'
+and `Description` = 'OBM'
+join 
 
-/*, 'Sls CD1 Desc#', 'Customer Number', 'Order Number', 'Foreign Extended Price', 'Quantity Shipped'
-'Computer Peripheral'
-'Mouse F.G.I.'  '19100542', 'SF2', '', 'Peru', '100314', 'EXPORT. IMPORT. 
-IGARASHI ASCEN', '100314', 'Igarashi', 'Mouse', 'Wireless Mouse', 'Computer Peripheral', 
-'Mouse F.G.I.', 'Wireless Mouse', 'NX-7000', 'Optical & Wirless Mouse', 
-'RS2,NX-7000,BLUE,G5,HANGER', '31030109109', '自製', '2020', '2', '30.25', 
-'4.1', '3280', '2307.52', '972.48', '800', 'OBM'
+;
 
+/*
+Order Number, Department,
+Continent, Country,
+Ship To Number, Ship_To AlphaName,
+Customer Number, Customer Name, 
+LargeCat, MediumCat, Sls CD1 Desc#, Sls CD2  Desc#, Sls CD3 Desc#, Sls CD4 Desc#, Sls CD9 Desc#, 
+Product Desc#,2nd Item Number,
+Year, Month, Exchange Rate, 
+Foreign Unit Price, 
+Foreign Extended Price,
+Foreign Extended Cost,
+Foreign Ext# Margin,
+Quantity Shipped,
+Description
 */
