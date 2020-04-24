3b367ad8e1d313e1f92bf94a3bd67ff729d3734a
ricky <ricky1229x@gmail.com>
2020-04-24 18:00:53 +0800

    20200424.c

---
+++ PathSearch.sql
@@ -0,0 +1,5 @@
+#查詢資料庫路徑
+SELECT @@datadir;
+show variables where variable_name LIKE '%dir';
+
+#路徑在 C:\ProgramData\MySQL\MySQL Server 8.0\Data\
--- test.sql
+++ test.sql
@@ -1,29 +1,39 @@
-SELECT d.`2nd Item Number` "Item Num" , d.`Customer Name`, d.Description,	#含空格欄位須用反引號  
-	   d.`Foreign Extended Price`  ,  d.`Quantity Shipped`
-       
+SELECT  d.`Order Number`, d.`Department`, 
+        d.`2nd Item Number` "Item Num" , d.`Customer Name`, d.Description,  
+	    d.`Foreign Extended Price`  ,  d.`Quantity Shipped` , d.`Exchange Rate`,
+        d.`Sls CD4 Desc#` , d.`Foreign Extended Cost`
+        
 FROM genius_data.genius_data_202002 as d
-where `Department` = 'SF1' & 'SF3'
+where `Department` in('SF4','SF5')
 and `Year` = '2020'
 and `Month` = '2'
 and `LargeCat` = 'Mouse'
 and `MediumCat` = 'Wireless Mouse'
 and `Description` = 'OBM'
-join 
-
 ;
 
 /*
-Order Number, Department,
-Continent, Country,
-Ship To Number, Ship_To AlphaName,
-Customer Number, Customer Name, 
-LargeCat, MediumCat, Sls CD1 Desc#, Sls CD2  Desc#, Sls CD3 Desc#, Sls CD4 Desc#, Sls CD9 Desc#, 
-Product Desc#,2nd Item Number,
-Year, Month, Exchange Rate, 
-Foreign Unit Price, 
-Foreign Extended Price,
-Foreign Extended Cost,
-Foreign Ext# Margin,
-Quantity Shipped,
-Description
+FROM genius_data.genius_data_202002 as d
+where `Department` in('SF4','SF5')
+and `Year` = '2020'
+and `Month` = '2'
+and `LargeCat` = 'Mouse'
+and `MediumCat` = 'Wireless Mouse'
+and `Description` = 'OBM'
+/*
+/*
+`Order Number`, `Department`,
+`Continent, Country`,
+`Ship To Number`, `Ship_To AlphaName`,
+`Customer Number`, `Customer Name`, 
+`LargeCat, MediumCat`, `Sls CD1 Desc#`, `Sls CD2  Desc#`, 
+`Sls CD3 Desc#`, `Sls CD4 Desc#`, `Sls CD9 Desc#`, 
+`Product Desc#`,`2nd Item Number`,
+`Year`, `Month`, `Exchange Rate`, 
+`Foreign Unit Price`, 
+`Foreign Extended Price`,
+`Foreign Extended Cost`,
+`Foreign Ext# Margin`,
+`Quantity Shipped`,
+`Description`
 */
