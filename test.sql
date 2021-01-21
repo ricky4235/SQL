fb3738f9b642478d928aa6f374ac56ec9974dff2
ricky <ricky1229x@gmail.com>
2021-01-21 11:09:14 +0800

    +

--- test.sql
+++ test.sql
@@ -1,10 +1,11 @@
-SELECT  d.Department, d.Country, d.`Customer Name`  
-		, CEILING(sum(`Revenue`)) as Revenue 
+SELECT  d.Department, d.Country, d.`Customer Name`
+		, CEILING(sum(`Revenue`)) as Revenue
         , row_count()
         
-FROM genius_data.`2010-2020` as d
-where `Year` = 2020
-and	Month = 12
-group by `Customer Name`
-order by Revenue desc
+FROM genius_data.`2020DA` as d
 
+where `Year` = 2020
+and	Month in(11, 12)
+group by `Customer Name`
+
+order by Revenue desc
